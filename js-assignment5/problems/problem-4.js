90d04406d0fcafe5ab4c56f40e1f6ecea644cbc4
Renee Edson <156867174+ReneeEdson@users.noreply.github.com>
2024-05-08 19:05:37 -0500

    Add files via upload

---
+++ js-assignment5/problems/problem-4.js
@@ -0,0 +1,44 @@
+
+/*
+Assignment 5: Problem 4 - Turn in the following array of arrays into a single object.
+The first element in each array is the key, and the second element is the value.
+
+You will also be asked to omit an element from the array.
+
+For example, the following array:
+
+const items = [
+	  [ 'name', 'Ford' ],
+	  [ 'model', 'Mustang' ],
+	  [ 'year', 1964 ], 	// Omit this element
+	  [ 'color', 'red' ]
+]
+
+Should be turned into the following object:
+{
+	name: 'Ford',
+	model: 'Mustang',
+	color: 'red'
+}
+
+TO TEST:
+You can run this example by running `node problems/problem-4` in your terminal,
+or you can run all by running `node run-all`.
+*/
+
+const { test } = require('../tester')
+
+const items = [
+	  [ 'firstName', 'Luke' ],
+	  [ 'lastName', 'Skywalker' ],
+	  [ 'age', 19 ],
+	  [ 'occupation', 'Jedi' ],
+	  [ 'homePlanet', 'Tatooine' ]
+]
+
+// Omit the 'age' element
+const filteredItems = items.filter(item => item[0] !== 'age')
+
+const person = Object.fromEntries(filteredItems) // append your code here
+
+test("Problem 4", person)
